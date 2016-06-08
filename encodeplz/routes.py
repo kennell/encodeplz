32cdf79fb437abea9705d952977f12bf3ea0b829
kennell <kevin@kennell.de>
2016-06-09 01:16:31 +0200

    return celery job id

--- encodeplz/routes.py
+++ encodeplz/routes.py
@@ -3,17 +3,15 @@ from encodeplz.jobs import do_something
 from encodeplz.utils import validate_json
 from flask import json, jsonify, request
 from uuid import uuid4
-
+from pprint import pprint
 
 @app.route('/jobs', methods=['PUT'])
 @validate_json
 def job_create():
     data = json.loads(request.data)
-    id = uuid4().hex
-    data['id'] = id
-    do_something.delay(data)
+    job = do_something.delay(data)
     return jsonify(
         {
-            'id': id
+            'id': job.id
         }
     )
