0e480e08484b5f234a5ae0f2bff2c13baddd86f4
kennell <kevin@kennell.de>
2016-08-09 15:43:53 +0200

    rework download handling

--- encodeplz/routes.py
+++ encodeplz/routes.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from encodeplz.app import app
 from encodeplz.jobs import transcode
 from encodeplz.utils import validate_json
-from flask import json, jsonify, request
+from flask import json, jsonify, request, send_file
 
 r = redis.StrictRedis(host='localhost', port=6379, db=3)
 
@@ -24,8 +24,17 @@ def job_create():
     )
 
 
-@app.route('/jobs/<job_id>')
+@app.route('/jobs/<job_id>', methods=['GET'])
 def job_show(job_id):
     return jsonify(
-        r.get(job_id)
+        {
+            'download_url': 'http://localhost:' + str(app.config['ENCODEPLZ_PORT']) + '/jobs/' + str(job_id) + "/download"
+        }
+    )
+
+
+@app.route('/jobs/<job_id>/download', methods=['GET'])
+def job_download(job_id):
+    return send_file(
+        r.get(job_id).decode(encoding='UTF-8')
     )
